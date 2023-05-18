c0b9ec972c22bb15a53610e8bcc546698a07d588
minkman <lomick2090@gmail.com>
2023-05-18 13:38:28 -0500

    add github link

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,6 +6,7 @@ export default function Contact() {
         <div className='contact'>
             <h1>Contact Me:</h1>
             <a href='mailto:lomick2090@gmail.com'><p>Email: Lomick2090@gmail.com</p></a>
+            <a href='https://github.com/lomick2090'><p>Github</p></a>
             <a href='https://www.linkedin.com/in/jesse-stahl-b5b980276/'><p>LinkedIn</p></a>
             <a href='https://www.facebook.com/jesse.stahl/'><p>Facebook</p></a>
         </div>
